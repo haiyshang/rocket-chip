68680e2be070138066af81a8ab391b6ec50d7233
Masayuki Kimura <masayuki@pezy.co.jp>
2017-09-21 01:59:58 +0900

    Func: temporary implement RoccMatrixTwoRequester

--- src/main/scala/rocket/rocc.scala
+++ src/main/scala/rocket/rocc.scala
@@ -418,3 +418,132 @@ class RoccCommandRouter(opcodes: Seq[OpcodeSet])(implicit p: Parameters)
   assert(PopCount(cmdReadys) <= UInt(1),
     "Custom opcode matched for more than one accelerator")
 }
+
+
+class MatrixMulTwoRequester(n: Int = 4)(implicit p: Parameters) extends RoCC()(p) {
+
+  val busy = Reg(init = {Bool(false)})
+
+  val setLength = funct === UInt(0)
+  val doCalc    = funct === UInt(1)
+
+  val r_cmd_count  = Reg(UInt(width = xLen))
+  val r_recv_count = Reg(UInt(width = xLen))
+
+  val r_matrix_max = Reg(UInt(width = xLen))
+
+  val r_resp_rd = Reg(io.resp.bits.rd)
+  val r_addr   = Reg(UInt(width = xLen))
+  val r_v_addr = Reg(UInt(width = xLen))
+  val r_h_addr = Reg(UInt(width = xLen))
+
+  // datapath
+  val r_total = Reg(UInt(width = xLen))
+  val r_tag = Reg(UInt(width = n))
+
+  val s_idle :: s_mem_fetch :: s_recv_finish :: s_mem_recv :: Nil = Enum(Bits(), 4)
+  val r_cmd_state  = Reg(UInt(width = 3), init = s_idle)
+  val r_recv_state = Reg(UInt(width = 3), init = s_idle)
+
+  when (io.cmd.valid) {
+    printf("MatrixMulTwoRequester: On Going. %x, %x\n", r_cmd_state, r_recv_state)
+  }
+
+
+  when (io.cmd.fire() && setLength) {
+    r_matrix_max = io.cmd.bits.rs1
+  }
+
+  when (io.cmd.fire() && doCalc) {
+    printf("MatrixMulTwoRequester: DoCalc Received. %x, %x\n", io.cmd.bits.rs1, io.cmd.bits.rs2)
+
+    r_total      := UInt(0)
+
+    r_v_addr     := io.cmd.bits.rs2
+    r_h_addr     := io.cmd.bits.rs1
+
+    r_recv_count := UInt(0)
+    r_cmd_count  := UInt(0)
+    r_tag        := UInt(0)
+
+    r_resp_rd := io.cmd.bits.inst.rd
+
+    r_cmd_state  := s_mem_fetch
+    r_recv_state := s_mem_recv
+  }
+
+  val w_addr = Mux (r_cmd_count(0) == UInt(0), r_h_addr, r_v_addr)
+
+  io.cmd.ready := (r_cmd_state === s_idle)
+  // command resolved if no stalls AND not issuing a load that will need a request
+
+  val cmd_finished = (r_cmd_count === ((r_matrix_max - UInt(1)) << UInt(1))
+  when ((r_cmd_state === s_mem_fetch) && io.mem.req.fire()) {
+    printf("MatrixMulTwoRequester: <<s_mem_fetch_v>> IO.MEM Command Fire %x\n", w_addr)
+
+    r_cmd_count  := Mux(cmd_v_finished, UInt(0), r_cmd_count + UInt(1))
+
+    r_h_addr     := Mux(r_cmd_count(0), r_h_addr + UInt(8), r_h_addr)
+    r_v_addr     := Mux(r_cmd_count(0), r_v_addr, r_v_addr + (r_matrix_max << UInt(3)))
+    r_cmd_state  := Mux(cmd_finished, s_idle, s_mem_fetch_v)
+  }
+
+  when (io.mem.req.fire()) {
+    r_tag        := r_tag + UInt(1)
+  }
+
+  // MEMORY REQUEST INTERFACE
+  io.mem.req.valid := (r_cmd_state === s_mem_fetch) || (r_cmd_state === s_mem_fetch_h)
+  io.mem.req.bits.addr := w_addr
+  io.mem.req.bits.tag  := r_tag
+  io.mem.req.bits.cmd  := M_XRD // perform a load (M_XWR for stores)
+  io.mem.req.bits.typ  := MT_D  // D = 8 bytes, W = 4, H = 2, B = 1
+  io.mem.req.bits.data := Bits(0) // we're not performing any stores...
+  io.mem.req.bits.phys := Bool(false)
+  io.mem.invalidate_lr := Bool(false)
+
+  val recv_v_finished = (r_recv_count === ((r_matrix_max - UInt(1)) << UInt(1))
+  when (r_recv_state === s_mem_recv && io.mem.resp.fire()) {
+    printf("MatrixMulTwoRequester: <<s_mem_recv_v>> IO.MEM Received %x\n", io.mem.resp.bits.data)
+
+    r_recv_count := Mux(recv_v_finished, UInt(0), r_recv_count + UInt(1))
+    r_recv_state := Mux(recv_v_finished, s_recv_finish, s_mem_recv)
+
+    r_h_val      := Mux(r_recv_count(0), io.mem.resp.bits.data, r_h_val)
+
+    r_total      := r_total + r_h_val * io.mem.resp.bits.data
+  }
+
+  val recv_h_finished = (r_recv_count === UInt(15))
+  when (r_recv_state === s_mem_recv_h && io.mem.resp.fire()) {
+    printf("MatrixMulTwoRequester: <<s_mem_recv_h>> IO.MEM Received %x\n", io.mem.resp.bits.data)
+
+    r_recv_count := Mux(recv_h_finished, UInt(0), r_recv_count + UInt(1))
+    r_recv_state := Mux(recv_h_finished, s_mem_recv_h)
+  }
+
+  // control
+  when (io.mem.req.fire()) {
+    busy := Bool(true)
+  }
+
+  when ((r_recv_state === s_recv_finish) && io.resp.fire()) {
+    r_recv_state := s_idle
+    printf("MatrixMulTwoRequester: Finished. Answer = %x\n", r_total)
+  }
+
+  // PROC RESPONSE INTERFACE
+  io.resp.valid := (io.cmd.fire() && setLength) ||   // setLength
+                   (r_recv_state === s_recv_finish)  // doCalc
+  // valid response if valid command, need a response, and no stalls
+  io.resp.bits.rd := r_resp_rd
+  // Must respond with the appropriate tag or undefined behavior
+  io.resp.bits.data := r_total
+  // Semantics is to always send out prior accumulator register value
+
+  io.busy      := Bool(false)
+  // io.busy := io.cmd.valid
+  // Be busy when have pending memory requests or committed possibility of pending requests
+  io.interrupt := Bool(false)
+  // Set this true to trigger an interrupt on the processor (please refer to supervisor documentation)
+}
