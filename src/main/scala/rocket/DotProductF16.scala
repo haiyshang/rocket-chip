80e6f411b7314a607da4b7c73ded465256949aa4
Masayuki Kimura <masayuki@pezy.co.jp>
2018-01-26 18:57:34 +0900

    Func: import 704fe4ab

--- src/main/scala/rocket/DotProductF16.scala
+++ src/main/scala/rocket/DotProductF16.scala
@@ -1,6 +1,8 @@
 package rocket
 
 import Chisel._
+import chisel3.core.{Input, Output}
+
 import uncore.tilelink._
 import uncore.constants._
 import uncore.agents.CacheName
@@ -8,8 +10,93 @@ import util._
 import Chisel.ImplicitConversions._
 import cde.{Parameters, Field}
 
-class DotProductF16(n: Int = 4)(implicit p: Parameters) extends RoCC()(p)
-  with HasCoreParameters {
+class Fix16Mul extends Module {
+  val io = IO(new Bundle {
+    val A_In   = Input(UInt(width = 32))
+    val B_In   = Input(UInt(width = 32))
+    val En_In  = Input(Bool())
+    val C_Out  = Output(SInt(width = 32))
+    val En_Out = Output(Bool())
+  })
+
+  val w_a_hi = Wire(SInt(width=32))
+  val w_b_hi = Wire(SInt(width=32))
+  val w_a_lo = Wire(UInt(width=32))
+  val w_b_lo = Wire(UInt(width=32))
+
+  // int32_t  A = (a_val >> 16),    C = (b_val >> 16);
+  // uint32_t B = (a_val & 0xFFFF), D = (b_val & 0xFFFF);
+  w_a_hi := io.A_In(31,16).asSInt()
+  w_b_hi := io.B_In(31,16).asSInt()
+  w_a_lo := Cat(UInt(0, 16), io.A_In(15, 0))
+  w_b_lo := Cat(UInt(0, 16), io.B_In(15, 0))
+
+  // int32_t  AC    = A*C;
+  // int32_t  AD_CB = A*D + C*B;
+  // uint32_t BD    = B*D;
+  val w_ah_bh       = Reg(SInt(width=32))
+  val w_ah_bl_al_bh = Reg(SInt(width=32))
+  val w_al_bl       = Reg(UInt(width=32))
+
+  w_ah_bh       := w_a_hi * w_b_hi
+  w_ah_bl_al_bh := w_a_hi * w_b_lo + w_a_lo * w_b_hi
+  w_al_bl       := w_a_lo * w_b_lo
+
+  val product_hi = Wire(SInt(width=32))
+  product_hi := w_ah_bh + w_ah_bl_al_bh(31,16).asSInt()
+
+  val product_lo = Wire(UInt(width=32))
+  product_lo := w_al_bl + Cat(w_ah_bl_al_bh, UInt(0,width=16))
+
+  val product_hi2 = Wire(SInt(width=32))
+  when (product_lo < w_al_bl) {
+    product_hi2 := product_hi + SInt(1)
+  } .otherwise {
+    product_hi2 := product_hi
+  }
+
+  val product_hi3 = Wire(SInt(width=32))
+  val product_lo2 = Wire(UInt(width=32))
+  product_lo2 := product_lo - UInt(0x8000) - product_hi(31)
+  when (product_lo2 > product_lo) {
+    product_hi3 := product_hi2 - SInt(1)
+  } .otherwise {
+    product_hi3 := product_hi2
+  }
+
+  io.C_Out := Cat(product_hi3(15, 0), product_lo2(31,16)).asSInt() + SInt(1)
+
+  val r_en_out = Reg(Bool())
+  r_en_out := io.En_In
+  io.En_Out := r_en_out
+}
+
+
+class LoggerRAM (DataWidth: Int, Length: Int) extends Module {
+  val io = IO(new Bundle {
+    val WData_In = Input(UInt(width = DataWidth))
+    val WAddr_In = Input(UInt(width = log2Ceil(Length)))
+    val We_In    = Input(Bool())
+
+    val RAddr_In = Input(UInt(width = log2Ceil(Length)))
+    val Data_Out = Output(UInt(width = DataWidth))
+  })
+
+  val memory = Mem(1024, UInt(width = DataWidth))
+
+  when (io.We_In) {
+    memory(io.WAddr_In) := io.WData_In
+  }
+
+  val w_data_out = Reg(UInt(width = DataWidth))
+  w_data_out := memory(io.RAddr_In)
+
+  io.Data_Out := w_data_out
+}
+
+
+class DotProductF16(n: Int = 4)(implicit p: Parameters) extends RoCC()(p) 
+{
   val busy = Reg(init = {Bool(false)})
 
   val funct   = io.cmd.bits.inst.funct
@@ -43,17 +130,13 @@ class DotProductF16(n: Int = 4)(implicit p: Parameters) extends RoCC()(p)
   val r_cmd_state  = Reg(UInt(width = 3), init = s_idle)
   val r_recv_state = Reg(UInt(width = 3), init = s_idle)
 
-  val w_ah_bh       = Reg(SInt(width=32))
-  val w_ah_bl_al_bh = Reg(SInt(width=32))
-  val w_al_bl       = Reg(UInt(width=32))
-
-  val r_recv_valid  = Reg(init = {Bool(false)})
+  val w_calc_done  = Wire(init = {Bool(false)})
 
   var logmem_word_len = 1024;
 
-  val result_regfile = Mem(logmem_word_len, UInt(width = 32))
-  val input_regfile  = Mem(logmem_word_len, UInt(width = 32))
-  val weight_regfile = Mem(logmem_word_len, UInt(width = 32))
+  val w_result_MemOutput  = Wire(UInt(width=32))
+  val w_input_MemOutput   = Wire(UInt(width=32))
+  val w_weight_MemOutput  = Wire(UInt(width=32))
 
   val r_result_log_count = Reg(UInt(width=32))
   val r_input_log_count  = Reg(UInt(width=32))
@@ -74,19 +157,19 @@ class DotProductF16(n: Int = 4)(implicit p: Parameters) extends RoCC()(p)
   }
 
   when (io.cmd.fire() && getResultMem) {
-    printf("DotProductF16: ResultMem[%d] = %x.\n", io.cmd.bits.rs1, result_regfile(r_log_addr))
+    printf("DotProductF16: ResultMem[%d]\n", io.cmd.bits.rs1)
     r_log_addr   := io.cmd.bits.rs1
     r_recv_state := s_recv_resultLog_finish
   }
 
   when (io.cmd.fire() && getInputMem) {
-    printf("DotProductF16: InputMem[%d] = %x.\n", io.cmd.bits.rs1, input_regfile(r_log_addr))
+    printf("DotProductF16: InputMem[%d].\n", io.cmd.bits.rs1)
     r_log_addr   := io.cmd.bits.rs1
     r_recv_state := s_recv_inputLog_finish
   }
 
   when (io.cmd.fire() && getWeightMem) {
-    printf("DotProductF16: WeightMem[%d] = %x.\n", io.cmd.bits.rs1, weight_regfile(r_log_addr))
+    printf("DotProductF16: WeightMem[%d].\n", io.cmd.bits.rs1)
     r_log_addr   := io.cmd.bits.rs1
     r_recv_state := s_recv_weightLog_finish
   }
@@ -109,6 +192,11 @@ class DotProductF16(n: Int = 4)(implicit p: Parameters) extends RoCC()(p)
 
     r_cmd_state  := s_mem_fetch
     r_recv_state := s_mem_recv
+
+    r_result_log_count := UInt(0)
+    r_input_log_count  := UInt(0)
+    r_weight_log_count := UInt(0)
+
   }
 
   val w_addr = Mux (r_cmd_count(0) === UInt(0), r_h_addr, r_v_addr)
@@ -152,81 +240,17 @@ class DotProductF16(n: Int = 4)(implicit p: Parameters) extends RoCC()(p)
     r_recv_state := Mux(recv_finished, s_recv_finish, s_mem_recv)
 
     r_a_val      := Mux(r_recv_count(0), r_a_val, io.mem.resp.bits.data)
-
-    when (r_recv_count(0)) {
-      r_recv_valid := UInt(1)
-    } .otherwise {
-      r_recv_valid := UInt(0)
-    }
-  } .otherwise {
-    r_recv_valid := UInt(0)
   }
 
-  when (r_recv_valid) {
+  when (w_calc_done) {
     r_total := r_total + w_result
     printf("DotProductF16: <<s_mem_recv_v>> w_result update %x\n", w_result)
   }
-
-  val w_a_val = Wire(UInt(width = 32))
-  val w_b_val = Wire(UInt(width = 32))
-  w_a_val := r_a_val
-  w_b_val := io.mem.resp.bits.data;
-
-  val w_a_hi = Wire(SInt(width=32))
-  val w_b_hi = Wire(SInt(width=32))
-  val w_a_lo = Wire(UInt(width=32))
-  val w_b_lo = Wire(UInt(width=32))
-
-  // int32_t  A = (a_val >> 16),    C = (b_val >> 16);
-  // uint32_t B = (a_val & 0xFFFF), D = (b_val & 0xFFFF);
-  w_a_hi := w_a_val(31,16).asSInt()
-  w_b_hi := w_b_val(31,16).asSInt()
-  w_a_lo := Cat(UInt(0, 16), w_a_val(15, 0))
-  w_b_lo := Cat(UInt(0, 16), w_b_val(15, 0))
-
-  // int32_t  AC    = A*C;
-  // int32_t  AD_CB = A*D + C*B;
-  // uint32_t BD    = B*D;
-  w_ah_bh       := w_a_hi * w_b_hi
-  w_ah_bl_al_bh := w_a_hi * w_b_lo + w_a_lo * w_b_hi
-  w_al_bl       := w_a_lo * w_b_lo
-
-  val product_hi = Wire(SInt(width=32))
-  product_hi := w_ah_bh + w_ah_bl_al_bh(31,16).asSInt()
-
-  val product_lo = Wire(UInt(width=32))
-  product_lo := w_al_bl + Cat(w_ah_bl_al_bh, UInt(0,width=16))
-
-  val product_hi2 = Wire(SInt(width=32))
-  when (product_lo < w_al_bl) {
-    product_hi2 := product_hi + SInt(1)
-  } .otherwise {
-    product_hi2 := product_hi
-  }
-
-  val product_hi3 = Wire(SInt(width=32))
-  val product_lo2 = Wire(UInt(width=32))
-  product_lo2 := product_lo - UInt(0x8000) - product_hi(31)
-  when (product_lo2 > product_lo) {
-    product_hi3 := product_hi2 - SInt(1)
-  } .otherwise {
-    product_hi3 := product_hi2
-  }
-
-  w_result := Cat(product_hi3(15, 0), product_lo2(31,16)).asSInt() + SInt(1)
 
   // control
   when (io.mem.req.fire()) {
     busy := Bool(true)
   }
-
-  val result_MemOutput = Wire(UInt(width = 32))
-  val input_MemOutput  = Wire(UInt(width = 32))
-  val weight_MemOutput = Wire(UInt(width = 32))
-
-  result_MemOutput := result_regfile(r_log_addr)
-  input_MemOutput  := input_regfile (r_log_addr)
-  weight_MemOutput := weight_regfile(r_log_addr)
 
   when (io.resp.fire()) {
     when (r_recv_state === s_recv_finish) {
@@ -234,13 +258,13 @@ class DotProductF16(n: Int = 4)(implicit p: Parameters) extends RoCC()(p)
       printf ("DotProductF16: Finished. Answer = %x\n", r_total)
     } .elsewhen (r_recv_state === s_recv_resultLog_finish) {
       r_recv_state := s_idle
-      printf ("DotProductF16: MemResult Finished = %x\n", result_MemOutput)
+      printf ("DotProductF16: MemResult Finished = %x\n", w_result_MemOutput)
     } .elsewhen (r_recv_state === s_recv_inputLog_finish) {
       r_recv_state := s_idle
-      printf ("DotProductF16: InputResult Finished = %x\n", input_MemOutput)
+      printf ("DotProductF16: InputResult Finished = %x\n", w_input_MemOutput)
     } .elsewhen (r_recv_state === s_recv_weightLog_finish) {
       r_recv_state := s_idle
-      printf ("DotProductF16: WeightResult Finished = %x\n", weight_MemOutput)
+      printf ("DotProductF16: WeightResult Finished = %x\n", w_weight_MemOutput)
     }
   }
 
@@ -252,13 +276,47 @@ class DotProductF16(n: Int = 4)(implicit p: Parameters) extends RoCC()(p)
   when (r_recv_state === s_recv_finish) {
     io.resp.bits.data := r_total.asUInt()
   } .elsewhen (r_recv_state === s_recv_resultLog_finish) {
-    io.resp.bits.data := result_MemOutput
+    io.resp.bits.data := w_result_MemOutput
   } .elsewhen (r_recv_state === s_recv_inputLog_finish) {
-    io.resp.bits.data := input_MemOutput
+    io.resp.bits.data := w_input_MemOutput
   } .elsewhen (r_recv_state === s_recv_weightLog_finish) {
-    io.resp.bits.data := weight_MemOutput
+    io.resp.bits.data := w_weight_MemOutput
   } .otherwise {
     io.resp.bits.data := UInt(0)
+  }
+
+  //=================================================
+  // Reorder Receiver
+  //=================================================
+  val data_tag_counter = Reg(UInt(width=n))
+
+  val a_data_queue = Module (new ReorderQueue(UInt(width=32), 2<<n))
+  val b_data_queue = Module (new ReorderQueue(UInt(width=32), 2<<n))
+
+  a_data_queue.io.enq.valid     := io.mem.resp.fire() && !io.mem.resp.bits.tag(0)
+  a_data_queue.io.enq.bits.data := io.mem.resp.bits.data
+  a_data_queue.io.enq.bits.tag  := io.mem.resp.bits.tag
+
+  a_data_queue.io.deq.tag  := Cat(data_tag_counter, UInt(0, width=1))
+
+  b_data_queue.io.enq.valid     := io.mem.resp.fire() &&  io.mem.resp.bits.tag(0)
+  b_data_queue.io.enq.bits.data := io.mem.resp.bits.data
+  b_data_queue.io.enq.bits.tag  := io.mem.resp.bits.tag
+
+  b_data_queue.io.deq.tag  := Cat(data_tag_counter, UInt(1, width=1))
+
+  when (a_data_queue.io.deq.matches && b_data_queue.io.deq.matches) {
+    a_data_queue.io.deq.valid := Bool(true)
+    b_data_queue.io.deq.valid := Bool(true)
+
+    data_tag_counter := data_tag_counter + UInt(1)
+  } .otherwise {
+    a_data_queue.io.deq.valid := Bool(false)
+    b_data_queue.io.deq.valid := Bool(false)
+  }
+
+  when (io.cmd.fire() && doCalc) {
+    data_tag_counter := UInt(0)
   }
 
   // Semantics is to always send out prior accumulator register value
@@ -267,49 +325,62 @@ class DotProductF16(n: Int = 4)(implicit p: Parameters) extends RoCC()(p)
   io.interrupt := Bool(false)
   // Set this true to trigger an interrupt on the processor (please refer to supervisor documentation)
 
+  val r_log_overflow = Reg(init = {Bool(false)})
+
+  val we_resultram = w_calc_done && (r_log_overflow === Bool(false))
+  val we_inputram  = (r_recv_state === s_mem_recv) && io.mem.resp.fire() && !r_recv_count(0)
+  val we_weightram = (r_recv_state === s_mem_recv) && io.mem.resp.fire() &&  r_recv_count(0)
+
+  //=================================================
+  // Fix16_Mul
+  //=================================================
+  val fix16_mul = Module (new Fix16Mul())
+  fix16_mul.io.A_In  := a_data_queue.io.deq.data
+  fix16_mul.io.B_In  := b_data_queue.io.deq.data
+  fix16_mul.io.En_In := a_data_queue.io.deq.matches && b_data_queue.io.deq.matches
+  w_result           := fix16_mul.io.C_Out
+  w_calc_done        := fix16_mul.io.En_Out
 
   //=================================================
   // Logger
   //=================================================
-  val result_regfile_in = Wire(SInt(width=32))
-  val r_log_overflow = Reg(init = {Bool(false)})
 
-  result_regfile_in := r_total + w_result
-  when (r_recv_valid && r_log_overflow === Bool(false)) {
-    result_regfile(r_result_log_count) := result_regfile_in.asUInt()
+  val ResultRAM = Module (new LoggerRAM(32, 1024))
+  val InputRAM  = Module (new LoggerRAM(32, 1024))
+  val WeightRAM = Module (new LoggerRAM(32, 1024))
 
-    printf("ResultMem[%x] = %x\n", r_result_log_count, r_result_log_count)
+  ResultRAM.io.WData_In := (r_total + w_result).asUInt()
+  ResultRAM.io.We_In    := we_resultram
+  ResultRAM.io.WAddr_In := r_result_log_count
+  ResultRAM.io.RAddr_In := r_log_addr
+  w_result_MemOutput    := ResultRAM.io.Data_Out
 
+  InputRAM.io.WData_In  := io.mem.resp.bits.data
+  InputRAM.io.We_In     := we_inputram
+  InputRAM.io.WAddr_In  := r_input_log_count
+  InputRAM.io.RAddr_In  := r_log_addr
+  w_input_MemOutput     := InputRAM.io.Data_Out
+
+  WeightRAM.io.WData_In := io.mem.resp.bits.data
+  WeightRAM.io.We_In    := we_weightram
+  WeightRAM.io.WAddr_In := r_weight_log_count
+  WeightRAM.io.RAddr_In := r_log_addr
+  w_weight_MemOutput    := WeightRAM.io.Data_Out
+
+  when (w_calc_done && r_log_overflow === Bool(false)) {
     when (r_result_log_count === UInt(logmem_word_len-1)) {
       r_log_overflow := Bool(true)
-    } .otherwise {
-      r_result_log_count := r_result_log_count + UInt(1)
     }
   }
 
-  // Input Logger
-  when (r_recv_state === s_mem_recv && io.mem.resp.fire() && !r_recv_count(0)) {
-    input_regfile(r_input_log_count) := io.mem.resp.bits.data
-
+  when (we_resultram) {
+    r_result_log_count := r_result_log_count + UInt(1)
+  }
+  when (we_inputram) {
     r_input_log_count := r_input_log_count + UInt(1)
-
-    printf("InputMem[%x] = %x\n", r_input_log_count, io.mem.resp.bits.data)
   }
-
-  // Weight Logger
-  when (r_recv_state === s_mem_recv && io.mem.resp.fire() &&  r_recv_count(0)) {
-    weight_regfile(r_weight_log_count) := io.mem.resp.bits.data
-
+  when (we_weightram) {
     r_weight_log_count := r_weight_log_count + UInt(1)
-
-    printf("InputMem[%x] = %x\n", r_weight_log_count, io.mem.resp.bits.data)
   }
 
-  when (io.cmd.fire() && doCalc) {
-    r_result_log_count := UInt(0)
-    r_input_log_count  := UInt(0)
-    r_weight_log_count := UInt(0)
-
-    r_log_overflow   := Bool(false)
-  }
 }
