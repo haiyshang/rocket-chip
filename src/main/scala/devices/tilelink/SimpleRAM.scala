cbe731f612c14626463c84a217db0a8b2e8e6b0f
Masayuki Kimura <masayuki@pezy.co.jp>
2018-02-14 23:59:39 +0900

    Func: update SimpleRAM to read/write

--- src/main/scala/devices/tilelink/SimpleRAM.scala
+++ src/main/scala/devices/tilelink/SimpleRAM.scala
@@ -29,8 +29,8 @@ class TLSimpleRAM(c: SimpleRAMParams)(implicit p: Parameters) extends LazyModule
     Seq(TLManagerParameters(
       address            = AddressSet.misaligned(c.address, c.depth*beatBytes),
       resources          = new SimpleDevice("ram", Seq("msyksphinz,simpleRAM")).reg("mem"),
-      regionType         = RegionType.UNCACHED,
-      executable         = true,
+      regionType         = RegionType.UNCACHEABLE,
+      // executable         = true,
       supportsGet        = TransferSizes(1, beatBytes),
       supportsPutPartial = TransferSizes(1, beatBytes),
       supportsPutFull    = TransferSizes(1, beatBytes),
@@ -39,37 +39,50 @@ class TLSimpleRAM(c: SimpleRAMParams)(implicit p: Parameters) extends LazyModule
 
   lazy val module = new LazyModuleImp(this) {
     val memory = Mem(1024, UInt(width = c.width))
+    val a_legal = Wire(Bool(true))
 
     val (in, edge) = node.in(0)
     val mem_address = edge.addr_hi(in.a.bits.address - UInt(c.address))(log2Ceil(c.depth)-1, 0)
 
-    val read = (in.a.bits.opcode === TLMessages.Get)
-    when (in.a.fire() && !read) {
-      memory(mem_address) := in.a.bits.data
-      // memory(mem_address) := 0.U
-    }
-
     val d_full = RegInit(Bool(false))
+    val d_read = Reg(Bool())
     val d_size = Reg(UInt())
     val d_source = Reg(UInt())
     val d_data = memory(mem_address) holdUnless RegNext(in.a.fire())
+    val d_legal = Reg(Bool())
 
     // Flow control
-    when (in.d.fire())         { d_full := Bool(false) }
-    when (in.a.fire() && read) { d_full := Bool(true)  }
+    when (in.d.fire()) { d_full := Bool(false) }
+    when (in.a.fire()) { d_full := Bool(true)  }
     in.d.valid := d_full
     in.a.ready := in.d.ready || !d_full
 
+    in.d.bits := edge.AccessAck(d_source, d_size, !d_legal)
+    // avoid data-bus Mux
+    in.d.bits.data := d_data
+    in.d.bits.opcode := Mux(d_read, TLMessages.AccessAckData, TLMessages.AccessAck)
+
+    val read = in.a.bits.opcode === TLMessages.Get
+    val rdata = Wire(Vec(beatBytes, Bits(width = 8)))
+    val wdata = Vec.tabulate(beatBytes) { i => in.a.bits.data(8*(i+1)-1, 8*i) }
+
     when (in.a.fire()) {
+      d_read   := read
       d_size   := in.a.bits.size
       d_source := in.a.bits.source
+      d_legal  := a_legal
     }
 
-    in.d.bits := edge.AccessAck(d_source, d_size, d_data)
 
     // Tie off unused channels
     in.b.valid := Bool(false)
     in.c.ready := Bool(true)
     in.e.ready := Bool(true)
+
+
+    when (in.a.fire() && !read && a_legal) {
+      memory(mem_address) := in.a.bits.data
+      // memory(mem_address) := 0.U
+    }
   }
 }
